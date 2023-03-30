7cf4bea5e5ce68d495710da5f0b10315f53c7a97
bogdan karpus <bigkpr@gmail.com>
2023-03-30 15:30:18 +0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { invoke } from "@tauri-apps/api/tauri";
+import "./App.css";
+
+function App() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    setGreetMsg(await invoke("greet", { name }));
+  }
+
+  return (
+    <div className="container">
+      <h1>Доброго вечора, ми з України!</h1>
+
+      <div className="row">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            greet();
+          }}
+        >
+          <input
+            id="greet-input"
+            onChange={(e) => setName(e.currentTarget.value)}
+            placeholder="Напиши команду та нажми відправити"
+          />
+          <button type="submit">Відправити</button>
+        </form>
+      </div>
+      <p>{greetMsg}</p>
+    </div>
+  );
+}
+
+export default App;
